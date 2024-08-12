1746daab7db6e6fa1a6303abdd83d2461377815e
uwimpuhweruth <uwimpuhwe2006@gmail.com>
2024-08-12 19:22:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//document.getElementById("count-el") . innerText = 5
+
+let count = 567
+
+console.log(count)
+
+
+// console.log(myAge)
+ 
+// 1.Create a variable, myAge, and set its value to your age
+let myAge = 45 
+
+// 2. Log the myAge variable in the console
+console.log(myAge)
